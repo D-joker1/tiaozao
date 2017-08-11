ae806b5bd0d3f4d7cb865633875b5fc647bf5c60
maomao1026 <sayaka1026@163.com>
2017-08-11 17:49:51 +0800

    修复了未尽兴验证就可进行下一步的bug

--- tiaozao/mobile_register_ViewController.swift
+++ tiaozao/mobile_register_ViewController.swift
@@ -27,6 +27,7 @@ class mobile_register_ViewController: UIViewController {
 
 
     @IBAction func error(_ sender: Any) {
+        var flag = 0
         //没有填写手机号
         let phoneNumber = mobile.text
         let mobileNumber = "^(13[0-9]|15[0-9]|18[0-9]|17[0-9]|147)\\d{8}$"
@@ -43,7 +44,15 @@ class mobile_register_ViewController: UIViewController {
             let action2 = UIAlertAction(title:"确定",style:UIAlertActionStyle.default,handler:nil)
             alertController2.addAction(action2)
             self.present(alertController2,animated:true,completion: nil)
-        }else{
+        }
+        else if(flag == 0){
+            let alertController2 = UIAlertController(title:"提示",message:"未进行验证或验证码输入错误！",preferredStyle:UIAlertControllerStyle.alert)
+            let action2 = UIAlertAction(title:"确定",style:UIAlertActionStyle.default,handler:nil)
+            alertController2.addAction(action2)
+            self.present(alertController2,animated:true,completion: nil)
+
+        }
+        else{
             let sb = UIStoryboard(name:"Main", bundle: nil)
             let vc1 = sb.instantiateViewController(withIdentifier: "zhuce_xuexiaozhuce") as UIViewController
             self.present(vc1, animated: true, completion: nil)
@@ -54,6 +63,15 @@ class mobile_register_ViewController: UIViewController {
             let detail_vc = mimashezhi_View()
             self.present(detail_vc, animated: true, completion: nil)
         }
+        SMSSDK.commitVerificationCode("86", phoneNumber: mobile.text, zone: code.text) { (error) in
+            if error == nil {
+                print("验证成功")
+                flag = 1
+            } else {
+                print("验证失败")
+                flag = 0
+            }
+        }
     }
 
     @IBAction func sendCode(_ sender: UIButton) {
@@ -62,10 +80,15 @@ class mobile_register_ViewController: UIViewController {
                 print("获取验证码成功")
             } else {
                 print("错误描述\(String(describing:error))")
+                    let alertController1 = UIAlertController(title:"提示",message:"验证码发送失败",preferredStyle:UIAlertControllerStyle.alert)
+                    let action1 = UIAlertAction(title:"确定",style:UIAlertActionStyle.default,handler:nil)
+                    alertController1.addAction(action1)
+                    self.present(alertController1,animated:true,completion: nil)
             }
         }
     }
-    @IBAction func sumbitCode(_ sender: UIButton) {
+}
+/*    @IBAction func sumbitCode(_ sender: UIButton) {
         SMSSDK.commitVerificationCode("86", phoneNumber: mobile.text, zone: code.text) { (error) in
             if error == nil {
                 print("验证成功")
@@ -73,7 +96,7 @@ class mobile_register_ViewController: UIViewController {
                 print("验证失败")
             }
         }
-    }
+    }*/
     
     
  /*
@@ -85,4 +108,4 @@ class mobile_register_ViewController: UIViewController {
         // Pass the selected object to the new view controller.
 }
  */
-}
+
